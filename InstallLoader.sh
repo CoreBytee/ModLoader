b3da256e2f68ca49ac705f627f33367d67a72de5
ScriptItWithCode <62032154+ScriptItWithCode@users.noreply.github.com>
2021-02-10 13:51:07 +0100

    Update InstallLoader.sh

--- InstallLoader.sh
+++ InstallLoader.sh
@@ -5,7 +5,11 @@ DownloadURL=https://github.com/CoreBytee/ModLoader/archive/main.zip
 curl -L $DownloadURL > loader.zip
 
 unzip loader.zip
+
+echo Removing ZIP
 rm loader.zip
+
+echo Renaming
 mv ModLoader-main/ ModLoader
 
 echo Done
