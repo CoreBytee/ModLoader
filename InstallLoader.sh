4b0d61a8462052793a34dc9670daa91dd66a357d
ScriptItWithCode <62032154+ScriptItWithCode@users.noreply.github.com>
2021-02-10 13:54:42 +0100

    yes?

--- InstallLoader.sh
+++ InstallLoader.sh
@@ -13,3 +13,13 @@ echo "Renaming"
 mv ModLoader-main/ ModLoader
 
 echo "Done"
+
+read -p "Wil je de Loader openen? (y/n):" -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo Okey!
+
+
+    ./ModLoader/Open.sh
+
+fi
