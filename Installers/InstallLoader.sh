efd238d25d69e9dd0c80fd51f66069c4e1b57020
ScriptItWithCode <62032154+ScriptItWithCode@users.noreply.github.com>
2021-02-11 10:20:17 +0100

    zzz

--- Installers/InstallLoader.sh
+++ Installers/InstallLoader.sh
@@ -4,6 +4,7 @@ DownloadURL=https://github.com/CoreBytee/ModLoader/archive/main.zip
 echo removing old..
 rm -r ModLoader/
 
+sleep 2
 
 echo "Downloading"
 
@@ -13,16 +14,26 @@ curl -L $DownloadURL > Loader.zip
 
 unzip Loader.zip
 
+sleep 1
+
 echo "Removing ZIP"
 rm Loader.zip
 
+sleep 1
+
 echo "Renaming"
 mv ModLoader-main/ ModLoader
+
+sleep 1
 
 cd ModLoader
 mkdir Luvit
 cd Luvit
 
+sleep 1
+
 curl -L https://github.com/luvit/lit/raw/master/get-lit.sh | sh
+
+sleep 3
 
 echo "Done"
