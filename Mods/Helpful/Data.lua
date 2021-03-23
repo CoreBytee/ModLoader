a0bd109d3459fe83009ef1bef0d568d222515bc4
ScriptItWithCode <62032154+CoreBytee@users.noreply.github.com>
2021-03-23 08:34:48 +0100

    data helper

---
+++ Mods/Helpful/Data.lua
@@ -0,0 +1,25 @@
+return {
+
+    Name = "Helpful mods",
+    Desc = "Mods die handig zijn als je aan het spelen bent\nWordt niet veel gebruikt ingame!",
+
+    Files = {
+        FabricMods = {
+
+            ModMenu = {
+                Name = "Mod menu",
+                Desc = "Handig om te kijken welke mods je hebt\nMeestal Gebruikt om te configureren.",
+                File = "Mod-Menu.jar",
+            },
+
+            ModUpdater = {
+                Name = "ModUpdater",
+                Desc = "Ik weet zelf niet wat deze mod doet maar hij is wel handig",
+                File = "Mod-Updater.jar"
+            }
+
+        },
+        NonFabricMods = {}
+    }
+
+}
