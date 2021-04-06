dda7a2e644d735ee6cdbd756cf74ab23c364e252
ScriptItWithCode <62032154+ScriptItWithCode@users.noreply.github.com>
2021-04-06 11:02:09 +0200

    quotes

--- Installers/InstallLoader.sh
+++ Installers/InstallLoader.sh
@@ -1,7 +1,7 @@
 Name=ModLoader
 DownloadURL=https://github.com/CoreBytee/ModLoader/archive/main.zip
 
-cd ~/Library/Application Support/minecraft
+cd "~/Library/Application Support/minecraft"
 
 echo removing old..
 rm -r ModLoader/
