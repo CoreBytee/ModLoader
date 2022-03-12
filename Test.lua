f2b253780756a46a8eaf42ba4be94d5ab241d368
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-03-12 13:18:48 +0100

    thats something

--- Test.lua
+++ Test.lua
@@ -1,38 +1,39 @@
-
---[[
-local function AdvancedRequest(Url)
-    local ParsedUrl = require("http").parseUrl(Url)
-    local Response
-    local Data
-    local function Callback (ReturnData)
-        Data = ReturnData
-        p(Data)
-    end
-    if string.sub(Url, 1, 5) == "https" then
-        Response = require("https").get(ParsedUrl, function () end)
-    else
-        Response = require("http").get(ParsedUrl, function () end)
-    end
-
-    Response:once("finish", Callback)
-    Response.socket:on("data", function (A)
-        print(A)
-    end)
-    repeat
-        require("timer").sleep(50)
-    until Done
-    p(Data.headers["Content-Length"])
-    
-    if Data.statusCode == 302 or Data.statusCode == 301 then
-        print("Advanced Request > Redirected")
-        return AdvancedRequest(Data.headers.Location)
-    end
-    p(1)
-    return Data
-end
-]]
-
 local Sleep = require("timer").sleep
+
+local function PrintBar(Percent, Name, Stage, Length)
+    local LoadedLength = math.floor((Length / 100) * Percent)
+    local Bar = ""
+
+    for Index = 1, Length do
+        if Index <= LoadedLength then
+            Bar = Bar .. "#"
+        else
+            Bar = Bar .. "-"
+        end
+    end
+
+    _G.process.stdout.handle:write(
+        string.format(
+            "\r%s: [%s](%s%%)(%s)                  ",
+            Name,
+            string.format("\27[%im", 36) .. Bar .. "\27[0m",
+            math.floor(Percent),
+            Stage
+        )
+    )
+
+    if Percent >= 100 then
+        _G.process.stdout.handle:write("\n")
+    end
+end
+
+local function GetHeader(HeaderName, Headers)
+    for Index, Header in ipairs(Headers) do
+        if Header[1] == HeaderName then
+            return Header[2]
+        end
+    end
+end
 
 local function AdvancedRequest(Url)
     local ParsedUrl = require("http").parseUrl(Url)
@@ -40,11 +41,32 @@ local function AdvancedRequest(Url)
     local Request
 
     local Response
+    local Body = ""
 
     local function Callback(CurrentResponse)
         Response = CurrentResponse
-        p(CurrentResponse)
-        CurrentResponse:on("data", print)
+        local ContentLength = tonumber(GetHeader("Content-Length", Response.headers) or 0)
+        CurrentResponse:on(
+            "data",
+            function (Data)
+                Body = Body .. Data
+                Response:resume()
+            end
+        )
+        coroutine.wrap(function ()
+            while Finished == false do
+                PrintBar(#Body / ContentLength * 100, "Downloading mods", "", 100)
+                Sleep(3000)
+            end
+        end)()
+        
+        CurrentResponse:on(
+            "end",
+            function ()
+                print()
+                Finished = true
+            end
+        )
     end
 
     if string.sub(Url, 1, 5) == "https" then
@@ -53,23 +75,15 @@ local function AdvancedRequest(Url)
         Request = require("http").get(ParsedUrl, Callback)
     end
 
-    Request:on("finish", function ()
-        coroutine.wrap(function ()
-            repeat
-                Sleep(50)
-            until Response
-            Finished = true
-        end)()
-    end)
-
-
     repeat
         Sleep(50)
     until Finished == true
     if Response.statusCode == 302 or Response.statusCode == 301 then
         print("AdvancedRequest > Redirected")
-        AdvancedRequest(Response.headers.Location)
+        return AdvancedRequest(Response.headers.Location)
     end
+
+    return Response, Body
 end
 
 AdvancedRequest("http://mods.corebyte.ga")
