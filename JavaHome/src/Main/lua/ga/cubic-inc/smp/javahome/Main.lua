3f288634c023641a31f5b2ea1a6fa49b43103a07
ScriptItWithCode <62032154+CoreBytee@users.noreply.github.com>
2022-05-25 11:44:36 +0200

    ext

--- JavaHome/src/Main/lua/ga/cubic-inc/smp/javahome/Main.lua
+++ JavaHome/src/Main/lua/ga/cubic-inc/smp/javahome/Main.lua
@@ -1,4 +1,6 @@
 local FS = require("fs")
 
-FS.writeFileSync(TypeWriter.ExeFolder .. "/JavaHome.txt", Import("ga.cubic-inc.smp.javahome.Find")())
-p(TypeWriter.ExeFolder)
+FS.writeFileSync(
+    TypeWriter.ExeFolder .. "/JavaHome.txt",
+    Import("ga.cubic-inc.smp.javahome.Find")() .. "/bin/java" .. (({["win32"] = ".exe"})[TypeWriter.Os] or "")
+)
