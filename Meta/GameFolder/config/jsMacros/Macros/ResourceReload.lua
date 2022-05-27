525e1157d6b1e6cd28f42cf14a522807990cb6c9
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-05-27 16:39:26 +0200

    nre mods

--- Meta/GameFolder/config/jsMacros/Macros/ResourceReload.lua
+++
@@ -1,4 +0,0 @@
-print((event or {isGameStart = false}).isGameStart)
-
-FS:open("../../../CubicLoader/PlayerName.txt"):write(Player:getPlayer():getName():getString())
-print(JsMacros:open("../../../CubicLoader/CubicModReporter.exe"))
