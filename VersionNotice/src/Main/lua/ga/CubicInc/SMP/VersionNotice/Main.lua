95ee724917aa2438d3d1f5fed488fb4dc5fb6a39
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-05-27 09:59:55 +0200

    change to error

--- VersionNotice/src/Main/lua/ga/CubicInc/SMP/VersionNotice/Main.lua
+++ VersionNotice/src/Main/lua/ga/CubicInc/SMP/VersionNotice/Main.lua
@@ -1,8 +1,8 @@
 print("Hello World!")
 
 print()
-local Warning = TypeWriter.Logger.Warn
-Warning("A error occured and the process has ended")
-Warning("Error: Version out of date")
-Warning("Please reinstall CubicLoader by downloading the installer and running it")
+local Error = TypeWriter.Logger.Error
+Error("A error occured and the process has ended")
+Error("Error: Version out of date")
+Error("Please reinstall CubicLoader by downloading the installer and running it")
 Wait(5000000)
