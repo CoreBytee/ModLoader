1d13a5cec8f742e5ba5ca9787996dac8282b8c62
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-05-27 08:56:53 +0200

    fix stuff

--- JavaHome/src/Main/lua/ga/cubic-inc/smp/javahome/Main.lua
+++ JavaHome/src/Main/lua/ga/cubic-inc/smp/javahome/Main.lua
@@ -1,6 +1,28 @@
 local FS = require("fs")
 
+local File = Import("ga.cubic-inc.smp.javahome.Find")() .. "/bin/java" .. (({["win32"] = ".exe"})[TypeWriter.Os] or "")
+
 FS.writeFileSync(
     TypeWriter.ExeFolder .. "/JavaHome.txt",
-    Import("ga.cubic-inc.smp.javahome.Find")() .. "/bin/java" .. (({["win32"] = ".exe"})[TypeWriter.Os] or "")
+    File
 )
+
+local Spawn = require("coro-spawn")
+
+local Result = Spawn(
+    File,
+    {
+        args = {
+            "-version"
+        }
+    }
+)
+Result.waitExit()
+local Version = Result.stderr.read():split("\n")[1]:split("\"")[2]
+
+if Version:sub(0, 2) ~= "17" then
+    print("false")
+    return
+end
+
+print("true")
