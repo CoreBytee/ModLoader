119be17a797601754a8ca2117dad209e97f06cb9
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-05-30 21:46:26 +0200

    Remove version notice because its not being used anymore

--- VersionNotice/src/Main/lua/ga/CubicInc/SMP/VersionNotice/Main.lua
+++
@@ -1,8 +0,0 @@
-print("Hello World!")
-
-print()
-local Error = TypeWriter.Logger.Error
-Error("A error occured and the process has ended")
-Error("Error: Version out of date")
-Error("Please reinstall CubicLoader by downloading the installer and running it")
-Wait(5000000)
